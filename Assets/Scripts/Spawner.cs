ad2508ff6f965a3ec1615832214408836e3aaa27
JazzyLucas <59397873+JazzyLucas@users.noreply.github.com>
2024-02-16 19:16:43 -0500

    update spawner, meshutils, block_cheese, rat, cheesetest

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,28 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using Utils;
 
 public class Spawner : MonoBehaviour
 {
     [field: SerializeField] public GameObject ObjectToSpawn { get; private set; }
     [field: SerializeField] public List<Transform> SpawnPositions { get; private set; }
-    [field: SerializeField] public uint NumberOfObjects { get; private set; } = 10;
     [field: SerializeField] private float spawnRadius { get; set; } = 3;
     public float SpawnRadius => Mathf.Abs(spawnRadius);
+    [field: SerializeField] private bool isSphere { get; set; } = false;
+    [field: SerializeField] private bool onOutside { get; set; } = false;
+    [field: SerializeField] private bool randomRotation { get; set; } = false;
+    [field: Header("Debug")]
+    [field: SerializeField] private Color debugColor { get; set; }
+    public Color DebugColor => new(debugColor.r, debugColor.g, debugColor.b, 1);
 
     private void Start()
     {
-        SpawnObjects();
+        SpawnObjects(15); // TODO: call this in a manager (or something else)
     }
 
-    private void SpawnObjects()
+    public void SpawnObjects(int amount)
     {
-        for (int i = 0; i < NumberOfObjects; i++)
+        for (int i = 0; i < amount; i++)
         {
             var randomT = SpawnPositions[Random.Range(0, SpawnPositions.Count)];
-            var randomPos = Random.insideUnitSphere * SpawnRadius + randomT.position;
-            var randomRot = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
-            Instantiate(ObjectToSpawn, randomPos, randomRot);
+            Vector3 pos;
+            if (isSphere)
+            {
+                pos = (onOutside ? Random.onUnitSphere : Random.insideUnitSphere) * SpawnRadius + randomT.position;
+            }
+            else
+            {
+                var circle = (onOutside ? Random.insideUnitCircle.normalized : Random.insideUnitCircle) * SpawnRadius;
+                pos = new Vector3(circle.x, 0, circle.y) + randomT.position;
+            }
+            Quaternion rot = randomRotation ? Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)) : Quaternion.identity;
+            Instantiate(ObjectToSpawn, pos, rot);
         }
     }
     
@@ -30,8 +46,16 @@ public class Spawner : MonoBehaviour
     {
         foreach (var pos in SpawnPositions)
         {
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawWireSphere(pos.position, SpawnRadius);
+            Gizmos.color = DebugColor;
+            if (isSphere)
+            {
+                Gizmos.DrawWireSphere(pos.position, SpawnRadius);
+            }
+            else
+            {
+                var mesh = MeshUtils.GetUnityPrimitiveMesh(PrimitiveType.Cylinder);
+                Gizmos.DrawWireMesh(mesh, pos.position, Quaternion.identity, new(spawnRadius*2, 0 ,spawnRadius*2));
+            }
         }
     }
 }
---
+++ Assets/Scripts/Utils/MeshUtils.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils
+{
+    public static class MeshUtils
+    {
+	    private static Mesh _unityCapsuleMesh = null;
+	    private static Mesh _unityCubeMesh = null;
+	    private static Mesh _unityCylinderMesh = null;
+	    private static Mesh _unityPlaneMesh = null;
+	    private static Mesh _unitySphereMesh = null;
+	    private static Mesh _unityQuadMesh = null;
+	    
+        public static Mesh GetUnityPrimitiveMesh(PrimitiveType primitiveType)
+        {
+	        return primitiveType switch
+	        {
+		        PrimitiveType.Sphere => GetCachedPrimitiveMesh(ref _unitySphereMesh, primitiveType),
+		        PrimitiveType.Capsule => GetCachedPrimitiveMesh(ref _unityCapsuleMesh, primitiveType),
+		        PrimitiveType.Cylinder => GetCachedPrimitiveMesh(ref _unityCylinderMesh, primitiveType),
+		        PrimitiveType.Cube => GetCachedPrimitiveMesh(ref _unityCubeMesh, primitiveType),
+		        PrimitiveType.Plane => GetCachedPrimitiveMesh(ref _unityPlaneMesh, primitiveType),
+		        PrimitiveType.Quad => GetCachedPrimitiveMesh(ref _unityQuadMesh, primitiveType),
+		        _ => throw new ArgumentOutOfRangeException(nameof(primitiveType), primitiveType, null)
+	        };
+        }
+
+		private static Mesh GetCachedPrimitiveMesh(ref Mesh primMesh, PrimitiveType primitiveType)
+		{
+			if (primMesh == null)
+			{
+				Debug.Log("Getting Unity Primitive Mesh: " + primitiveType);
+				primMesh = Resources.GetBuiltinResource<Mesh>(GetPrimitiveMeshPath(primitiveType));
+
+				if (primMesh == null)
+				{
+					Debug.LogError("Couldn't load Unity Primitive Mesh: " + primitiveType);
+				}
+			}
+
+			return primMesh;
+		}
+
+		private static string GetPrimitiveMeshPath(PrimitiveType primitiveType)
+		{
+			return primitiveType switch
+			{
+				PrimitiveType.Sphere => "New-Sphere.fbx",
+				PrimitiveType.Capsule => "New-Capsule.fbx",
+				PrimitiveType.Cylinder => "New-Cylinder.fbx",
+				PrimitiveType.Cube => "Cube.fbx",
+				PrimitiveType.Plane => "New-Plane.fbx",
+				PrimitiveType.Quad => "Quad.fbx",
+				_ => throw new ArgumentOutOfRangeException(nameof(primitiveType), primitiveType, null)
+			};
+		}
+    }
+}
