1e7567ec0530b99eea2590d738e7f9447e109c65
Controlplaza <43657657+Controlplaza@users.noreply.github.com>
2024-02-18 18:38:23 -0500

    starting audio stuff

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+   [Header("Music")]
+   [SerializeField] private  AudioSource _backGroundMusic;
+
+    [Header("Weapons")]
+    [SerializeField] private AudioSource _ballSound;
+    [SerializeField] private AudioSource _toothPickImpact;
+}
